252f63cc07fbf840d4f2c0b38b0bc284899bd19b
Lauren Bobo <ldb44912@uga.edu>
2022-10-29 12:07:16 -0400

    figured out how to count line numbers in head

--- head.c
+++ head.c
@@ -4,45 +4,72 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-
+#define true 1
+#define false 0
+#define BUFFSIZE 514
 int main(int argc, char* argv[]) {
-    int * BUFFSIZE;
-    int readFile;
-
+    int readFile = 0;
     int c,n = 10;
     int indexOfFileStart = 1;
     int opt;
     while((opt = getopt(argc,argv, ":n:c:")) != -1) {
-        if (opt == 'n') {
+        switch(opt) {
+        case 'n': {
             n = atoi(optarg);
             indexOfFileStart = 3;
-        } //if
-        if(opt == 'c') {
+            break;
+        } //case n
+        case 'c': {
             c = atoi(optarg);
             indexOfFileStart = 3;
             n = 0;
-            BUFFSIZE = (int*)malloc(c);
-        } //if
+            break;
+        } //case c
+        } //switch
     } //while
-
+    //set variables for file read
+    int i = indexOfFileStart;
     //print number of bytes
     if (c > 0) {
         //start after command line arguments
-        int i = indexOfFileStart;
+        //loop through all provided file names
         for( i ; i < argc; i++) {
             char * fileName = argv[i];
             int file = open(fileName, O_RDONLY);
+            //set buffer to size c
             char buffer[c];
             if (file == -1) perror("open");
-        //read from file at argv[i] and print to std output
+        //read from file at argv[i] for only c bytes and print to std output
             readFile = read(file, buffer, c);
             //printf("readfile = %d", readFile);
             write(STDOUT_FILENO, buffer, readFile);
             if(readFile == -1) perror("read");
         } //for
-    } //if c
-        // print num lines
+    } //if
+
+
+    // print num lines
         if (n > 0) {
+            int i = indexOfFileStart;
+            for( i ; i < argc; i++) {
+                char * fileName = argv[i];
+                int file = open(fileName, O_RDONLY);
+                char buffer[BUFFSIZE];
+                if (file == -1) perror("open");
+                //read file and print it until lines exceed n
+                readFile = read(file, buffer, BUFFSIZE);
+                int lineNum = 0;
+                int j = 0;
+                for( j;  j < readFile && lineNum < n; j++) {
+                    if(buffer[j] == '\n') {
+                        lineNum++;
+                        printf("%d", lineNum);
+                        write(STDOUT_FILENO, buffer, readFile);
+                    } //if
+                }
+                              // } //for
+                if(readFile == -1) perror("read");
+            } //for
 
         } //if n
 } //main
