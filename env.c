50c5fbff95dfcf07f5d1ee849617fdc1f308ff2b
Lauren Bobo <ldb44912@uga.edu>
2022-11-01 11:20:48 -0400

    added int main to env

---
+++ env.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <errno.h>
+
+#define true 1
+#define false 0
+#define BUFFSIZE 514
+int main(int argc, char* argv[]) {
+} //main
