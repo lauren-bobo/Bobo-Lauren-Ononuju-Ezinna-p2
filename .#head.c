5d44ad7b3eddfc54b1740c9055c8aae5451b551f
Lauren Bobo <ldb44912@uga.edu>
2022-11-01 21:28:23 -0400

    wrote test scripts

--- .#head.c
+++
@@ -1,1 +0,0 @@
-ldb44912@csci-odin.cs.uga.edu.21182:1638798037
---
+++ StandardInputTest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+echo "standard input"
+ ./head
+ head
+
+ ./tail -
+ tail -
+
+ ./head -n 5
+ head-n5
+
+ ./tail -c 5 -
+ tail -c 5
+
+ ./head -n 5 file1.txt -
+ head -n 5 file1.txt -
+
+ ./tail -c 5 - file2.txt
+ tail -c 5 - file2.txt
+
+ ./wc
+ wc
+
+ ./wc -
+ wc -
+
+ ./wc -lw -
+ wc -lw -
+
+ ./wc -c -w file1.txt - file2.txt
+wc -c -w file1.txt - file2.txt
